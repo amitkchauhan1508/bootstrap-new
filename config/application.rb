4c74e5749a9e80b7038e78d7f562c4b18f47c118
Amit Kumar <amitkchauhan1508@gmail.com>
2014-04-02 03:13:57 +0530

    New bootstrap demo-3

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,8 @@ Bundler.require(*Rails.groups)
 module BootstrapNew
   class Application < Rails::Application
     
+    config.assets.initialize_on_precompile = false
+    
     config.autoload_paths += %W(#{config.root}/lib)
     
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
